242c8f3b59e79f74d7f9baceeed0cd4ce8d69729
kaja2006 <kajanithiruvarangan@gmail.com>
2026-06-04 17:41:39 +0530

    first commit

---
+++ python learning day 04.py
@@ -0,0 +1,7 @@
+print ("im studying python for four days")
+print ("excercise 01")
+Force = float(input("enter the force: "))
+mass = float(input("enter the mass: "))
+acceleration = Force / mass
+print ("the acceleration is", acceleration)
+
