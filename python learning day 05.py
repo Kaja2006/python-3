0b3360be81029860e3275fe8143e5847633a4625
kaja2006 <kajanithiruvarangan@gmail.com>
2026-06-05 23:55:52 +0530

    first commit

---
+++ python learning day 05.py
@@ -0,0 +1,10 @@
+print ("im studying python for four days")
+print ("excercise 01")
+
+a=6
+b=9
+
+print ("Sum is", a+b)
+print ("Subtraction is", a-b)
+print ("multiplication is", a*b)
+print ("Division is", a/b)
