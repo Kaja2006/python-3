87dd95d905784c31af9cab948404970b78d1e04e
kaja2006 <kajanithiruvarangan@gmail.com>
2026-06-06 12:44:29 +0530

    second commit

--- python learning day 05.py
+++ python learning day 05.py
@@ -4,7 +4,12 @@ print ("excercise 01")
 a=6
 b=9
 
+print ("power is", a**b)
+print ("multiplication is", a*b)
+print ("division is", a/b)
+print ("Floor Division is", a//b)
+print ("Modulus is", a%b)
 print ("Sum is", a+b)
 print ("Subtraction is", a-b)
-print ("multiplication is", a*b)
-print ("Division is", a/b)
+
+
