efccd70c2511dffcf88c79c25ae857ba44cc8c71
kaja2006 <kajanithiruvarangan@gmail.com>
2026-06-11 23:24:43 +0530

    fifth commit

---
+++ python learning day 10.py
@@ -0,0 +1,14 @@
+print ("exercise 01")
+
+a=b
+b=5
+c=6
+
+print("d =",a+c)
+print("e=",a-c)
+print("f=",a*c)
+print("g=",a/c)
+print("h=",a//c)
+print("i=",a%c)
+print("j=",a**c)
+print("k=",a**(1/c))
