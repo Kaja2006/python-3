de18bd7a74ef98dabc538bb8e3d9bda4737b2f41
kaja2006 <kajanithiruvarangan@gmail.com>
2026-06-14 00:01:28 +0530

    seventh commit

---
+++ python learning day 12.py
@@ -0,0 +1,15 @@
+for i in range(1,11):
+    print(i)
+
+for i in range(0,25,5):
+    print(i)
+
+for i in range(10,0,-1):
+    print(i)
+
+for i in range(10,0,-1):
+    print(i)
+
+for i in range(9,0,-1):
+    print(i,i-9)
+    
